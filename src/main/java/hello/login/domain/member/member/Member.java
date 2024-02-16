5ec6a8ffde3ec0c0a941f481916918b1d9500bd2
hyuk <tig05205@naver.com>
2024-02-16 21:36:37 +0900

    login

---
+++ src/main/java/hello/login/domain/member/member/Member.java
@@ -0,0 +1,18 @@
+package hello.login.domain.member.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    @NotEmpty // 비어있으면 안됨
+    private String loginId; //로그인 ID
+    @NotEmpty
+    private String name; //사용자이름
+    @NotEmpty
+    private String password;
+}
